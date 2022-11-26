84ab9b178530653ba7b7981f9d7bb79a4dfad00a
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2022-11-26 23:57:13 +0100

    update env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.register({
+      secret,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
